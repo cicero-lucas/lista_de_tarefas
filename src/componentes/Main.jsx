6820cce1b9cfb8be9583c445527b63b6605854f7
cicero lucas <cicerolucassilva10@gmail.com>
2023-11-07 16:41:42 -0300

    Iniciando Projeto

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import {React,useState } from "react";
 
 export default function Main(){
+    const [tarefa,setTarefa]=useState([])
+    const [texto,setTexto]=useState('')
+
+    function addTarefa(form){
+        form.preventDefault()
+        setTarefa(texto)
+        setTexto('')
+        document.getElementById('itarefa').focus()
+        console.log(tarefa)
+        
+    }
+
     return(
         <>
             <main>
-                <section className="formulario">
+                <form className="formulario" onSubmit={(form)=>addTarefa(form)}>
                    <div className="campos">
-                    <input type="text" placeholder="Digite sua tarefa" />
+                    
+                    <input type="text" id="itarefa" placeholder="Digite sua tarefa" value={texto}  onChange={(text)=>setTexto(text.target.value)}/>
                    </div>
 
                    <div className="campos">
-                        <button>Adicionar Tarefa</button>
+                        <button type="submit">Adicionar Tarefa</button>
                    </div>
+                </form>
+
+                <section>
+                    {tarefa.length<1
+                        ?
+                        <div>
+                            <p>aaaaaaaaaaaaaaaaa</p>
+                        </div>
+                        :
+                        <section>
+                            {tarefa.map((el,index)=>(
+                                <p key={index}>{el}</p>
+                            ))}
+                        </section>
+                    }
                 </section>
+
+                
             </main>
         </>
     );
